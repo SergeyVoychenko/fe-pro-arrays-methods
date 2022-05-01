164dab75fee21b5aa89b38b6dd07aa853adab09a
Sergey Voychenko <s.vouchenko@gmail.com>
2022-05-01 15:31:57 +0300

    fix minPriceReducer

--- src/index.js
+++ src/index.js
@@ -73,11 +73,11 @@ const rangeFilter = (from, to) => {
 };
 
 const minPriceReducer = () => {
-  return goods.reduce((acum, item) => Math.min(acum, item.price));
+  return goods.reduce((minPrice, item) => minPrice < item.price ? minPrice : item.price);
  }
 
 const maxPriceReducer = () => {
-  return goods.reduce((acum, item) => acum < item.price ? item.price: acum, 0);
+  return goods.reduce((maxPrice, item) => maxPrice < item.price ? item.price: maxPrice, 0);
 }
 
 const toMaxSorter = () => { 
